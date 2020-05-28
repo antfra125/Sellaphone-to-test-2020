57b313d2408a986d2f8e202cca0a41e67e353d4d
Benjamin Berglund <benjaminberglund@gmail.com>
2020-05-28 13:38:48 +0200

    added data calculation

--- client/src/components/Data.vue
+++ client/src/components/Data.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="datas">
     <div class="data">
-      <input type="radio" name="data" value="50" placeholder="Data" />
+      <input type="radio" v-model="data" name="data" value="50" placeholder="Data" />
       <label>10 GB</label>
     </div>
     <div class="data">
-      <input type="radio" name="data" value="100" placeholder="Data" />
+      <input type="radio" v-model="data" name="data" value="100" placeholder="Data" />
       <label>25 GB</label>
     </div>
     <div class="data">
-      <input type="radio" name="data" value="150" placeholder="Data" />
+      <input type="radio" v-model="data" name="data" value="150" placeholder="Data" />
       <label>50 GB</label>
     </div>
     <div class="data">
-      <input type="radio" name="data" value="200" placeholder="Data" />
+      <input type="radio" v-model="data" name="data" value="200" placeholder="Data" />
       <label>100 GB</label>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed:{
+    data:{
+      get(){
+        return this.$store.state.data
+      },
+      set(value){
+        this.$store.commit('setData', value)
+        this.$store.commit('updateTotal')
+      }
+    }
+  }
+}
+</script>
